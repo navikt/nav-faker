79d6186ce8b135cbd0b89842018f4ceeec182b04
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-07-31 08:42:51 +0200

    Legg til støtte for å generere en dato for x-antall år siden

--- lib/dato/dato.ts
+++ lib/dato/dato.ts
@@ -1,3 +1,4 @@
+import moment = require('moment');
 import NavFaker from '../navfaker';
 
 class Dato {
@@ -8,11 +9,18 @@ class Dato {
         this.faker = faker;
     }
 
-    public mellom(fra: Date, til: Date) {
+    public mellom(fra: Date, til: Date): Date {
         const fraMilli = Date.parse(fra.toString());
         const dateOffset = this.faker.random.number(Date.parse(til.toString()) - fraMilli);
 
         return new Date(fraMilli + dateOffset);
+    }
+
+    public forÅrSiden(årSiden: number): Date {
+        const datoIKorrektÅr = moment().subtract(årSiden, 'years');
+        return this.mellom(
+            datoIKorrektÅr.startOf('year').toDate(),
+            datoIKorrektÅr.endOf('year').toDate());
     }
 
 }
