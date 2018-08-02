cb8bc13336094b931ab5363138aabd9988e8ccdb
Joakim Lindquister <joakim.lindquister@bekk.no>
2018-08-02 11:30:22 +0200

    feat(Person): Legger til funksjon for tilfeldig antall barn for et fødselsnummer
    Returnerer et tall mellom 0 og 15. Det er mer sannynlig å få et lavt antall barn (under 5).

--- lib/person/person.test.ts
+++ lib/person/person.test.ts
@@ -8,3 +8,23 @@ test('Returnerer kjønn', () => {
     expect(tilfeldigKjønn).toBeGreaterThanOrEqual(0);
     expect(tilfeldigKjønn).toBeLessThanOrEqual(1);
 });
+
+describe('antall barn', () => {
+
+    test('Tilfeldig antall', () => {
+        const faker = navfaker;
+
+        const antallBarn = faker.person.antallBarn(new Date('1950-01-01'));
+
+        expect(antallBarn).toBeGreaterThanOrEqual(0);
+    });
+
+    test('Barn har ikke barn', () => {
+        const faker = navfaker;
+
+        const antallBarn = faker.person.antallBarn(new Date());
+
+        expect(antallBarn).toEqual(0);
+    });
+
+});
