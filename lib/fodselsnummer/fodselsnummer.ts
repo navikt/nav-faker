7ae18348c88e780618343ac0e092d9c24ee80ac0
Joakim Lindquister <joakim.lindquister@nav.no>
2018-08-07 08:35:25 +0200

    fix(fødselsnummer): Feilfiks av genering av dnummer
    Genererer nå kun D-nummer med gyldige kontrollsiffere
    
    fix #11

--- lib/fodselsnummer/fodselsnummer.ts
+++ lib/fodselsnummer/fodselsnummer.ts
@@ -1,20 +1,12 @@
 import moment = require('moment');
 import NavFaker from '../navfaker';
-import { padLeftNumber } from '../utils/string-utils';
 import { getConfigOrDefault } from './helpers/config-helper';
 import { fødselsnummerTilDato } from './helpers/fodselsdato-beregner';
-import { beregnKontrollsiffer1, beregnKontrollsiffer2, datoSomStreng } from './helpers/fodselsnummer-utils';
+import FødselsnummerBeregner from './helpers/fodselsnummerBeregner';
 
 export enum Kjønn {
     KVINNE = 0,
     MANN = 1,
-}
-function erMellom(tall: number, min: number, max: number) {
-    return tall >= min && tall <= max;
-}
-
-function getLøpenummerSomListe(start: number, end: number) {
-    return Array(end - start + 1).fill(0).map((_, index: number) => padLeftNumber(start + index, 3));
 }
 
 class Fødselsnummer {
@@ -26,17 +18,13 @@ class Fødselsnummer {
     }
 
     public generer(fødselsdato?: Date, kjønn?: number): string {
-        const parsedOptions = getConfigOrDefault(this.faker, {fødselsdato, kjønn});
-        return this.genererTilfeldigFødselsnummer(parsedOptions.fødselsdato, parsedOptions.kjønn);
+        const parsedOptions = getConfigOrDefault(this.faker, {fødselsdato, kjønn}, false);
+        return new FødselsnummerBeregner(this.faker, parsedOptions).tilfeldigFødselsnummer();
     }
 
     public dnummer(fødselsdato?: Date, kjønn?: number): string {
-        const fødselsnummer = this.generer(fødselsdato, kjønn);
-
-        const førsteSiffer = Number(fødselsnummer.charAt(0));
-        const nyttFørstesiffer = førsteSiffer + 4;
-
-        return String(nyttFørstesiffer).concat(fødselsnummer.substring(1));
+        const parsedOptions = getConfigOrDefault(this.faker, {fødselsdato, kjønn}, true);
+        return new FødselsnummerBeregner(this.faker, parsedOptions).tilfeldigFødselsnummer();
     }
 
     public myndig(kjønn?: number): string {
@@ -50,57 +38,6 @@ class Fødselsnummer {
     public getFødselsdato(fødselsnummer: string): Date {
         return fødselsnummerTilDato(fødselsnummer);
     }
-
-    private genererTilfeldigFødselsnummer(dato: Date, kjønn: Kjønn) {
-        const individnummere = this.getGyldigeIndividnummere(dato, kjønn);
-
-        const formatertDato = datoSomStreng(dato);
-        const tilfeldigIndividNummer = individnummere[this.faker.random.number(individnummere.length - 1)];
-
-        const k1 = beregnKontrollsiffer1(formatertDato + tilfeldigIndividNummer);
-        const k2 = beregnKontrollsiffer2(formatertDato + tilfeldigIndividNummer + k1);
-
-        return formatertDato + tilfeldigIndividNummer + k1 + k2;
-    }
-
-    private getGyldigeIndividnummere(dato: Date, kjønn: Kjønn) {
-
-        function individsifferHarRiktigKjønn(individnummer: string) {
-            const kjønnSiffer = Number(individnummer.charAt(2));
-            return kjønnSiffer % 2 === kjønn;
-        }
-
-        function gyldigeKontrollsiffere(individnummer: string) {
-            const fødselsnummer = datoSomStreng(dato) + individnummer;
-            const k1 = beregnKontrollsiffer1(fødselsnummer);
-            if (k1 === 10) {
-                return false;
-            }
-            if (beregnKontrollsiffer2(fødselsnummer + k1) === 10) {
-                return false;
-            }
-            return true;
-        }
-
-        return this.getIndividsiffereForDato(dato)
-            .filter(individsifferHarRiktigKjønn)
-            .filter(gyldigeKontrollsiffere);
-    }
-
-    private getIndividsiffereForDato(date: Date) {
-        const year = date.getFullYear();
-
-        if (erMellom(year, 1854, 1899)) {
-            return getLøpenummerSomListe(500, 749);
-        } else if (erMellom(year, 1900, 1999)) {
-            return getLøpenummerSomListe(0, 500);
-        } else if (erMellom(year, 2000, 2039)) {
-            return getLøpenummerSomListe(500, 999);
-        } else {
-            throw new Error('Ugyldig dato');
-        }
-    }
-
 }
 
 export default Fødselsnummer;
